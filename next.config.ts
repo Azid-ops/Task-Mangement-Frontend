e2f787439cc58847da65e2de48ba5c829d147d49
Azid-ops <mahad5063@gmail.com>
2026-06-12 22:56:51 +0500

    Second

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        // Kubernetes mein yahan aapki Backend Service ka internal DNS aayega
+        destination: 'http://task-app-service:5000/:path*', 
+      },
+    ];
+  },
 };
 
 export default nextConfig;
